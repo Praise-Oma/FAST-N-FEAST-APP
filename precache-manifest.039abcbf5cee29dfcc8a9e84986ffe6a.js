6a13ab0a7f6af5c638f9ecf1295849dc4778766a
Praise <praiz_ohanele@yahoo.com>
2021-08-18 04:12:36 +0100

    Updates

---
+++ precache-manifest.039abcbf5cee29dfcc8a9e84986ffe6a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f554e3447c0e1e298e6c20bc3dd07ee2",
+    "url": "/FAST-N-FEAST-APP.git/index.html"
+  },
+  {
+    "revision": "854ae6adcc12c9f4dbba",
+    "url": "/FAST-N-FEAST-APP.git/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a84764e26d40aeca0df6",
+    "url": "/FAST-N-FEAST-APP.git/static/css/main.1db0c5d3.chunk.css"
+  },
+  {
+    "revision": "854ae6adcc12c9f4dbba",
+    "url": "/FAST-N-FEAST-APP.git/static/js/2.3e6516bf.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/FAST-N-FEAST-APP.git/static/js/2.3e6516bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a84764e26d40aeca0df6",
+    "url": "/FAST-N-FEAST-APP.git/static/js/main.3676f609.chunk.js"
+  },
+  {
+    "revision": "0cfc58ab599c41121cf2",
+    "url": "/FAST-N-FEAST-APP.git/static/js/runtime-main.5837df44.js"
+  }
+]);
